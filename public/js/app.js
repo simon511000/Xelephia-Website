011f97f5185be2a29abacb0f0bb56684bd08008e
Simon Ledoux <simon@simon511000.fr>
2020-05-03 09:02:58 +0200

    fix d'un bug (npm run watch s'executait à l'infini)

--- public/js/app.js
+++ public/js/app.js
@@ -49951,9 +49951,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\xelephia.old\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\laragon\www\xelephia.old\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\laragon\www\xelephia.old\resources\sass\templatemo-training-studio.scss */"./resources/sass/templatemo-training-studio.scss");
+__webpack_require__(/*! C:\laragon\www\xelephia\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\laragon\www\xelephia\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\laragon\www\xelephia\resources\sass\templatemo-training-studio.scss */"./resources/sass/templatemo-training-studio.scss");
 
 
 /***/ })
